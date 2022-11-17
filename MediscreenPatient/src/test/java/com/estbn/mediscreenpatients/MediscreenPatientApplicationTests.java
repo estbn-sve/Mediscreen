b3a6b3b94bef1b180923030af0a98a69c6ca1807
Esteban <est.savalle@gmail.com>
2022-11-17 11:05:41 +0100

    Init Commit

---
+++ MediscreenPatient/src/test/java/com/estbn/mediscreenpatients/MediscreenPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estbn.mediscreenpatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
